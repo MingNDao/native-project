26c79860aa9895d89995d595840c269ddb9c4ab4
lxms951104 <lxms.951104@gmail.com>
2018-09-01 21:03:46 +0800

    删除多余文件

--- android/app/src/main/java/com/nativeproject/MainActivity.java
+++ android/app/src/main/java/com/nativeproject/MainActivity.java
@@ -1,10 +1,6 @@
 package com.nativeproject;
 
 import com.facebook.react.ReactActivity;
-// import android.net.Uri;
-// import android.os.Handler;
-// import android.os.Bundle;
-// import com.nativeproject.SMSObserver
 
 public class MainActivity extends ReactActivity {
 
@@ -16,16 +12,5 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "nativeProject";
     }
-
-    // private SmsObserver smsObserver;  //监控数据库变化
-    // private Handler smsHandler = new Handler() {};
-
-    // @Override
-    // protected void onCreate(Bundle savedInstanceState) {
-    //     super.onCreate(savedInstanceState);
-    //     smsObserver = new SmsObserver(this, smsHandler);  //实例化内容观察者的子类
-    //     getContentResolver().registerContentObserver(Uri.parse("content://sms/"), true, smsObserver);  //注册内容观察者
-
-    // }
 
 }
